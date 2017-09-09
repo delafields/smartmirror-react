f85112284285243b18f4badec2b14948dc8d4e6f
badatfun <j.c.delafields@gmail.com>
2017-09-09 18:46:03 -0400

    Basic boilerplate done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
